e63c6dd01752d22194dc5f9a8f297223a4975cbc
snaens <samuelpelz007@gmail.com>
2025-01-11 23:14:18 +0100

    restructure

--- micro_rosso_battery_monitor.cpp
+++ micro_rosso_battery_monitor.cpp
@@ -1,8 +1,5 @@
-
 #include "micro_rosso.h"
-
-#include "micro_rosso_mlx90640.h"
-
+#include "micro_rosso_battery_monitor.h"
 #include <micro_ros_utilities/string_utilities.h>
 #include <sensor_msgs/msg/battery_state.h>
 
@@ -28,34 +25,34 @@ static publisher_descriptor pdescriptor_battery;
 
 Basic_Battery::Basic_Battery()
 {
-  msg_battery_state.header.frame_id = micro_ros_string_utilities_set(msg_image.header.frame_id, "base_link");
-  msg_battery_state.voltage = 0.f;
+  msg_battery_state.header.frame_id = micro_ros_string_utilities_set(msg_battery_state.header.frame_id, "base_link");
+  msg_battery_state.voltage = 1.f;
   //TODO ...
 };
 
 static void report_cb(int64_t last_call_time)
 {
-  if (pdescriptor_image.topic_name != NULL)
+  if (pdescriptor_battery.topic_name != nullptr)
   {
     micro_rosso::set_timestamp(msg_battery_state.header.stamp);
     RCNOCHECK(rcl_publish(
       &pdescriptor_battery.publisher,
       &msg_battery_state,
-      NULL));
+      nullptr));
   }
 }
 
 bool Basic_Battery::setup(const int analog_pin,
                             const char *topic_name,
-                            timer_descriptor &timer)
+                            timer_descriptor &timer) 
 {
-  if (topic_image != NULL)
+  if (topic_name != NULL)
   {
     pdescriptor_battery.qos = QOS_BEST_EFFORT;
     pdescriptor_battery.type_support =
       (rosidl_message_type_support_t *)ROSIDL_GET_MSG_TYPE_SUPPORT(
         sensor_msgs, msg, BatteryState);
-    pdescriptor_battery.topic_name = topic_bat;
+    pdescriptor_battery.topic_name = topic_name;
     micro_rosso::publishers.push_back(&pdescriptor_battery);
   }
 
